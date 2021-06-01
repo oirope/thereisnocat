d388d50706fe52c1c474a0ac4cc4129862ddd4d7
Oirope <50852017+oirope@users.noreply.github.com>
2021-05-31 22:41:42 -0500

    Implemented keyboard input

--- src/game.ts
+++ src/game.ts
@@ -1,20 +1,55 @@
-import { cursor } from 'ansi-escape-sequences';
-import { cursorTo } from 'readline';
+import { emitKeypressEvents } from 'readline';
+import onChange from 'on-change';
 import { Cat, createCat } from './cat';
 import { Gun, createGun } from './gun';
 
-export type Game = {
+enum Direction {
+	North,
+	East,
+	South,
+	West,
+}
+
+type Game = {
 	size: number;
+	facing: Direction;
 	cat: Cat;
 	gun: Gun;
 };
 
-export function createGame(initialSize: number): Game {
+export function createGame(size: number): Game {
 	return {
-		size: initialSize,
-		cat: createCat(initialSize),
-		gun: createGun(initialSize),
+		size: size,
+		facing: Direction.North,
+		cat: createCat(size),
+		gun: createGun(size),
 	};
 }
 
-export function playGame(game: Game) {}
+export function playGame(game: Game) {
+	emitKeypressEvents(process.stdin);
+	process.stdin.setRawMode(true);
+
+	process.stdin.on('keypress', (_, key) => {
+		if (key) {
+			switch (key.name) {
+				case 'a':
+					if (game.facing-- === Direction.North) game.facing = Direction.West;
+					break;
+				case 'd':
+					if (game.facing++ === Direction.West) game.facing = Direction.North;
+					break;
+				case 'c':
+					if (key.ctrl) process.exit();
+					break;
+			}
+		}
+	});
+
+	game = onChange(game, function () {
+		drawGame(this);
+	});
+	drawGame(game);
+}
+
+function drawGame(game: Game) {}
